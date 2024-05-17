dea54940f625e0e30ee0f0cf2902d1466580f8e4
hakantozkoparan <hakantozkoparan@icloud.com>
2024-05-17 17:24:48 +0300

    Admin tarafı hazırlanıyor.

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -45,6 +45,14 @@ app.MapControllerRoute(
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+app.UseEndpoints(endpoints =>
+{
 	endpoints.MapControllerRoute(
 	  name: "areas",
 	  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
